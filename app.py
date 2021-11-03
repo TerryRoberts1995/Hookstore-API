1b2fefa210e02edc30068e046ea678b44582a6e4
TerryRoberts1995 <terryjbroberts@yahoo.com>
2021-11-03 11:06:50 -0500

    Added db.Model, and ma.schema to app.py

--- app.py
+++ app.py
@@ -11,3 +11,27 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 CORS(app)
 
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False, unique=True)
+    author = db.Column(db.String, nullable=False)
+    review = db.Column(db.String(144), nullable=False)
+    genre = db.Column(db.String, nullable=True)
+
+    def __init__(self, title, author, review, genre):
+        self.title = title
+        self.author = author
+        self.review = review
+        self.genre = genre
+
+class BookSchema(ma.Schema):
+    class Meta: 
+        fields: ("id", "title", "author", "review", "genre")
+
+book_schema = BookSchema()
+multiple_book_schema = BookSchema(many=True)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
