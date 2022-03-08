61bcfbe5997a591cda082b259694b84ba06e0ff3
TerryRoberts1995 <terryjbroberts@yahoo.com>
2022-03-08 15:21:54 -0600

    inital heroku push

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "app.sqlite")
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
