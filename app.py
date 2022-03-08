403ea44220490d3876e8ea86d321f55b689a408c
TerryRoberts1995 <terryjbroberts@yahoo.com>
2022-03-08 17:21:27 -0600

    used environ too

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 import os
 config = dotenv_values('.env')
+os.environ['DATABASE_URL'] = config['DATABASE_URL']
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:" + config["DATABASE_STRING"]
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:" + os.environ.get('DATABASE_URL')
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 CORS(app)
